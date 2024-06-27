d185a055b5c7b2928cbceab11c007ec6041f6717
SabinaKarimova <sabina.karimova.000@gmail.com>
2024-06-28 01:42:16 +0400

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentsComponent } from './students/students.component';
+import { ExamsComponent } from './exams/exams.component';
+import { SubjectsComponent } from './subjects/subjects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'student', pathMatch: 'full' },
+  { path: 'student', component: StudentsComponent },
+  { path: 'exams', component: ExamsComponent },
+  { path: 'subject', component: SubjectsComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './students/students.component';
+import { SubjectsComponent } from './subjects/subjects.component';
+import { ExamsComponent } from './exams/exams.component';
+import { NewStudentComponent } from './students/new-student/new-student.component';
+import { NewSubjectComponent } from './subjects/new-subject/new-subject.component';
+import { NewExamComponent } from './exams/new-exam/new-exam.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent,
+    SubjectsComponent,
+    ExamsComponent,
+    NewStudentComponent,
+    NewSubjectComponent,
+    NewExamComponent
   ],
   imports: [
     BrowserModule,
